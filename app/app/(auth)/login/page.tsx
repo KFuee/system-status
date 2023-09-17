ed943e0d635df1467584a3488346b2c1e71a1ce2
KFuee <jblascoisazac@icloud.com>
2023-09-17 12:22:13 +0200

    feat: oauth github y preparado entorno multi-tenancy

---
+++ app/app/(auth)/login/page.tsx
@@ -0,0 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import LoginButton from "./login-button";
+import { Suspense } from "react";
+
+export default function LoginPage() {
+  return (
+    <Card className="max-w-md">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl">System status</CardTitle>
+        <CardDescription>
+          Inicia sesión en tu cuenta para ver el estado de tus servicios.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="mt-4">
+          <Suspense
+            fallback={
+              <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
+            }
+          >
+            <LoginButton />
+          </Suspense>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
