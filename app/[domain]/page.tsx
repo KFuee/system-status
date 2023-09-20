c5f2cc1700dcb34080c62effab4adabc4effc9f3
KFuee <jblascoisazac@icloud.com>
2023-09-20 10:16:11 +0200

    chore: bump next version 13.5.1

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -10,35 +10,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { getSiteData } from "@/lib/fetchers";
-import { useCallback } from "react";
-
-const initialData: Tracker[] = [
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "rose", tooltip: "Downtime" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "gray", tooltip: "Maintenance" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "emerald", tooltip: "Operational" },
-  { color: "yellow", tooltip: "Degraded" },
-  { color: "emerald", tooltip: "Operational" },
-];
 
 export default async function SiteHomePage({
   params: { domain },
@@ -53,19 +24,23 @@ export default async function SiteHomePage({
 
   return (
     <div className="flex flex-1 flex-col items-center w-full space-y-6 sm:p-6">
-      <div className="flex flex-row justify-between w-full sm:max-w-2xl px-6 pt-6 sm:px-0 sm:pt-0">
-        <div className="flex flex-row items-center justify-center">
-          <Avatar className="mr-4 h-6 w-6">
-            <AvatarImage
-              src={`https://avatar.vercel.sh/${data.name}.png`}
-              alt={data.name ?? "Logo"}
-            />
-            <AvatarFallback>SC</AvatarFallback>
-          </Avatar>
-          <h1 className="text-2xl font-bold">{data.name}</h1>
+      <div className="flex flex-col space-y-2 w-full sm:max-w-2xl">
+        <div className="flex flex-row justify-between px-6 pt-6 sm:px-0 sm:pt-0">
+          <div className="flex flex-row items-center justify-center">
+            <Avatar className="mr-4 h-6 w-6">
+              <AvatarImage
+                src={`https://avatar.vercel.sh/${data.name}.png`}
+                alt={data.name ?? "Logo"}
+              />
+              <AvatarFallback>SC</AvatarFallback>
+            </Avatar>
+            <h1 className="text-2xl font-bold">{data.name}</h1>
+          </div>
+
+          <UiModeToggle />
         </div>
 
-        <UiModeToggle />
+        <p className="px-6 sm:px-0 sm:pb-0">{data.description}</p>
       </div>
 
       <Card className="rounded-none sm:rounded-lg w-full sm:max-w-2xl">
